b437fd24b614446a0441738c76105b338d75e01d
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-06 18:37:49 +0300

    time zone fix.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import inspect
 import models
 import pycodestyle
-import time
 import unittest
 from unittest import mock
 BaseModel = models.base_model.BaseModel
@@ -82,14 +81,13 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
-        tic = datetime.now()
+        tic = datetime.utcnow()
         inst1 = BaseModel()
-        toc = datetime.now()
+        toc = datetime.utcnow()
         self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
+        tic = datetime.utcnow()
         inst2 = BaseModel()
-        toc = datetime.now()
+        toc = datetime.utcnow()
         self.assertTrue(tic <= inst2.created_at <= toc)
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)

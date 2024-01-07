d27bbc44ec6530fa310fb27b287b7c0bb61673f6
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-07 18:00:46 +0300

    get and count methods update on branch storage_get_count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,21 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        if cls and id and cls in classes:
+            key = cls + '.' + id
+            obj = self.__objects.get(key, None)
+            return obj
+        else:
+            return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        total = 0
+        if cls in classes:
+            total = len(self.all(cls))
+        elif cls is None:
+            total = len(self.__objects)
+        return total

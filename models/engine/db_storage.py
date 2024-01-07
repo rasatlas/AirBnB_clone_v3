0aad0a9c64f0431a69f4e0c8aa37ceb6cc960067
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-07 18:55:05 +0300

    pycodestyle fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,13 +6,13 @@ import models
 from models.amenity import Amenity
 from models.base_model import Base
 from models.city import City
+from sqlalchemy import create_engine
+from os import getenv
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import User
-from os import getenv
-from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models.user import User
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}

4ca3cf753ff80d16443d5ef1b5cd96fb1efc304e
READMEmaybe <haroune11411@gmail.com>
2024-01-08 10:29:42 +0100

    Add task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,27 @@
 """ Blueprint for a flask app """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ Returns a JSON """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Returns a JSON """
+    dict = {}
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    for key, value in classes.items():
+        dict[key] = storage.count(value)
+    return jsonify(dict)

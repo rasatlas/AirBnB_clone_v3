be055eace0146d237fb1a5d7f3733cdd1825bbfa
READMEmaybe <haroune11411@gmail.com>
2024-01-08 04:20:56 +0100

    Add task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Blueprint for a flask app """
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ Returns a JSON """
+    return jsonify({"status": "OK"})

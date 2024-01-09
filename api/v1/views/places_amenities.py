593121210476a2cd54682b9bb63a2cb6f2a92c37
READMEmaybe <haroune11411@gmail.com>
2024-01-09 02:30:09 +0100

    debug

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,6 +1,6 @@
 #!!/usr/bin/python3
 """ places_amenities view """
-from flask import jsonify, abort, request
+from flask import jsonify, abort
 from models import storage
 from models.place import Place
 from models.amenity import Amenity
@@ -24,50 +24,3 @@ def retrieve_all_place_amenities(place_id):
         amenity_list.append(amenity.to_dict())
     return jsonify(amenity_list)
 
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def delete_place_amenity(place_id, amenity_id):
-    """
-    Deletes a Amenity object or raise 404 error if not found
-    Returns an empty dictionary with the status code 200
-    """
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenity = place.amenities
-    else:
-        place_amenity = place.amenity_ids
-    if amenity not in place_amenity:
-        abort(404)
-    storage.delete(amenity)
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['POST'], strict_slashes=False)
-def create_place_amenity(place_id, amenity_id):
-    """
-    Creates a Amenity
-    Returns the new Amenity with the status code 201
-    """
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenity = place.amenities
-    else:
-        place_amenity = place.amenity_ids
-    if amenity in place_amenity:
-        return jsonify(amenity.to_dict()), 200
-    place_amenity.append(amenity)
-    place.save()
-    return jsonify(amenity.to_dict()), 201
